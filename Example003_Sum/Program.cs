8212ebe0a2ac03b2bd273b234d4e6d41696cfe06
Timur <t1m.1h@yandex.ru>
2022-04-27 15:50:28 +1200

    correct code (6:result)

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 19);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
-Console.WriteLine (numberA + numberB);
+Console.WriteLine (result);
  
